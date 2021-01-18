1209c48879c7f6cc6e3e7d3a2d4e5e4b3dbbb239
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-18 21:22:35 +0000

    Added IntelRealSense + ROS Package
    Added IntelRealSense + ROS Package

--- init.sh
+++ init.sh
@@ -2,8 +2,10 @@
 sudo apt update && upgrade -y
 sudo apt autoremove -y
 
+
 ## Argon One Util
 curl https://download.argon40.com/argon1.sh | bash
+
 
 ## Install Software
 sudo apt install xrdp -y
@@ -11,8 +13,10 @@ sudo apt install xscreensaver -y
 sudo apt install blender -y
 sudo apt install arduino -y
 
+
 ## Instal Qt5
 sudo apt install qtcreator qt5-default -y
+
 
 ## Install ROS NOETIC
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu buster main" > /etc/apt/sources.list.d/ros-noetic.list'
@@ -50,6 +54,50 @@ cd
 sudo rm -r -f ros_catkin_ws
 
 # test
-source /opt/ros/noetic/setup.bash
-roscd
+#source /opt/ros/noetic/setup.bash
+#roscd
 #roscore
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
+## Intel RealSense
+wget https://github.com/IntelRealSense/librealsense/raw/master/scripts/libuvc_installation.sh
+chmod +x ./libuvc_installation.sh
+./libuvc_installation.sh
+sudo rm -r -f libuvc_installation.sh
+
+# Wait until Librealsense script completed message is displayed (may take some time)
+# Connect RealSense device
+# Run 'rs-enumerate-devices' from the terminal to verify the installation
+
+# Installing ROS Package: realsense-ros
+# source ROS
+source /opt/ros/noetic/setup.bash
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src/
+mv * ~/catkin_ws/src/realsense/
+cd ~/catkin_ws/src/
+git clone https://github.com/IntelRealSense/realsense-ros.git
+cd realsense-ros/
+git checkout 'git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1'
+cd ..
+catkin_init_workspace
+cd ..
+
+cd src
+git clone https://github.com/pal-robotics/ddynamic_reconfigure.git
+cd ddynamic_reconfigure
+git checkout 'git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1'
+cd ../..
+
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# test
+#roslaunch realsense2_camera rs_camera.launch
