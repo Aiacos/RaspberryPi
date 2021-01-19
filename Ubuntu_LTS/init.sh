a7938e458dffe35da043690b63cfc55346b3417a
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-19 22:53:47 +0000

    Update Realsense ROS install

--- Ubuntu_LTS/init.sh
+++ Ubuntu_LTS/init.sh
@@ -37,12 +37,36 @@ chmod +x ./libuvc_installation.sh
 ./libuvc_installation.sh
 sudo rm -r -f libuvc_installation.sh
 
-# ROS RealSense
-export REALSENSE_ROS_WS=$HOME/catkin_ws/
-sudo apt install ros-noetic-ddynamic-reconfigure -y
-git clone https://github.com/IntelRealSense/realsense-ros.git $REALSENSE_ROS_WS/src/realsense-ros
-cd $REALSENSE_ROS_WS
-catkin_make
+# Wait until Librealsense script completed message is displayed (may take some time)
+# Connect RealSense device
+# Run 'rs-enumerate-devices' from the terminal to verify the installation
+
+# Installing ROS Package: realsense-ros
+# source ROS
+source /opt/ros/noetic/setup.bash
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src/
+mv * ~/catkin_ws/src/realsense/
+cd ~/catkin_ws/src/
+git clone https://github.com/IntelRealSense/realsense-ros.git
+cd realsense-ros/
+git checkout 'git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1'
+cd ..
+catkin_init_workspace
+cd ..
+
+cd src
+git clone https://github.com/pal-robotics/ddynamic_reconfigure.git
+cd ddynamic_reconfigure
+git checkout 'git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1'
+cd ../..
+
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
 
 ## Environment setup
