038b0403b251a0a816a38d8c712d6615e0ced972
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-20 21:43:22 +0000

    ROS Dependencies for building packages

--- Ubuntu_LTS/init.sh
+++ Ubuntu_LTS/init.sh
@@ -24,8 +24,16 @@ sudo apt install qtcreator qt5-default -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
-sudo apt update
+sudo apt update -y
 
+# Dependencies for building packages
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential y
+sudo apt install python3-rosdep -y
+
+sudo rosdep init
+rosdep update
+
+# ROS
 sudo apt install ros-noetic-desktop-full -y
 
 
