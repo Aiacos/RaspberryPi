3921f6d0b294078fcad55ff5866330afeba85606
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-20 20:09:09 +0000

    Install Lubuntu as Desktop Enviroment

--- Ubuntu_LTS/init.sh
+++ Ubuntu_LTS/init.sh
@@ -3,15 +3,16 @@ sudo apt update && upgrade -y
 sudo apt autoremove -y
 
 ## Settings
-sudo timedatectl set-timezone Europe/London
-sudo apt install htpdate -y
+#sudo timedatectl set-timezone Europe/London
+#sudo apt install htpdate -y
 
 ## Argon One Util
 curl https://download.argon40.com/argon1.sh | bash
 
 ## Install GUI
-sudo apt install ubuntu-budgie-desktop -y
-sudo snap install ubuntu-budgie-welcome --classic
+#sudo apt install ubuntu-budgie-desktop -y
+#sudo snap install ubuntu-budgie-welcome --classic
+sudo apt install lubuntu-desktop -y
 
 ## Install Software
 sudo apt install xrdp -y
