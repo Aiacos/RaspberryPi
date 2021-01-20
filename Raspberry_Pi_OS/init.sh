917a8fa3f5802e1e0e80de4cd86228a1c2455657
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-20 08:41:14 +0000

    Update Raspberry Pi OS: ROS desktop_full

--- Raspberry_Pi_OS/init.sh
+++ Raspberry_Pi_OS/init.sh
@@ -39,12 +39,12 @@ cd ~/ros_catkin_ws
 #wstool init src noetic-ros_comm-wet.rosinstall
 
 # Desktop
-rosinstall_generator desktop --rosdistro noetic --deps --wet-only --tar > noetic-desktop-wet.rosinstall
-wstool init -j8 src noetic-desktop-wet.rosinstall
+#rosinstall_generator desktop --rosdistro noetic --deps --wet-only --tar > noetic-desktop-wet.rosinstall
+#wstool init -j8 src noetic-desktop-wet.rosinstall
 
-# Desktop-Full
-#rosinstall_generator desktop-full --rosdistro noetic --deps --wet-only --tar > noetic-desktop-full-wet.rosinstall
-#wstool init -j8 src noetic-desktop-full-wet.rosinstall
+# Desktop_Full
+rosinstall_generator desktop_full --rosdistro noetic --deps --wet-only --tar > noetic-desktop_full-wet.rosinstall
+wstool init -j8 src noetic-desktop_full-wet.rosinstall
 
 rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
 
