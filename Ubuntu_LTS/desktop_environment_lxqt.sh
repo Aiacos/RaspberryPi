59620bd5e5b76852f944d79258986b23e9c195a5
Lorenzo <uni.lorenzo.a@gmail.com>
2021-01-21 09:02:47 +0000

    desktopify

--- Ubuntu_LTS/desktop_environment_lxqt.sh
+++
@@ -1,8 +0,0 @@
-## Install
-sudo apt install tasksel -y
-
-## Install Lubuntu
-sudo tasksel install lubuntu-desktop
-
-
-sudo reboot
---
+++ Ubuntu_LTS/desktopify.sh
@@ -0,0 +1,20 @@
+## Install Desktop
+git clone https://github.com/wimpysworld/desktopify.git
+cd desktopify
+sudo ./desktopify --de ubuntu-kylin
+
+#Usage
+#  ./desktopify --de <desktop environment>
+
+#Available desktop environments are
+#  lubuntu
+#  kubuntu
+#  ubuntu
+#  ubuntu-budgie
+#  ubuntu-kylin
+#  ubuntu-mate
+#  ubuntu-studio
+#  xubuntu
+
+#You can also pass the optional --oem option which will run a setup
+#wizard on the next boot.
