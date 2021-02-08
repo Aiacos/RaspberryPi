b55950ee1888b99e1105fc2c036cc0f35827dfe1
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2021-02-08 23:07:37 +0000

    Update Ubuntu to be easier

--- Ubuntu_LTS/init.sh
+++ Ubuntu_LTS/init.sh
@@ -33,56 +33,50 @@ sudo apt update && sudo apt install codium -y
 
 
 ## Install ROS
+# ROS version:
+export ROS_VER=noetic
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update -y
 
 # ROS
-sudo apt install ros-noetic-desktop-full -y
+sudo apt install ros-$ROS_VER-desktop-full -y
 
 
 ## Install PS3 Controller librarie
-sudo apt-get install ros-noetic-joystick-drivers -y
+sudo apt-get install ros-$ROS_VER-joystick-drivers -y
 
 
 ## Install MoveIt
-sudo apt install ros-noetic-moveit -y
+sudo apt install ros-$ROS_VER-moveit -y
 
 
 ## Install Intel RealSense
-cd $home
-wget https://github.com/IntelRealSense/librealsense/raw/master/scripts/libuvc_installation.sh
-chmod +x ./libuvc_installation.sh
-./libuvc_installation.sh
-
-# Wait until Librealsense script completed message is displayed (may take some time)
-# Connect RealSense device
-# Run 'rs-enumerate-devices' from the terminal to verify the installation
-
-# Installing ROS Package: realsense-ros
-cd $home
-bash RaspberryPi/build/ros-realsense.sh
-
+sudo apt install ros-$ROS_VER-realsense2-camera -y
+sudo apt install ros-$ROS_VER-realsense2-description -y
 
 ## Install Arduino ROS Lib
-sudo apt-get install ros-noetic-rosserial-arduino -y
-sudo apt-get install ros-noetic-rosserial -y
+sudo apt-get install ros-$ROS_VER-rosserial-arduino -y
+sudo apt-get install ros-$ROS_VER-rosserial -y
 
+## Arduino IDE Setup
 cd $home
-bash RaspberryPi/build/ros-serial-arduino.sh
+mkdir -p ~/sketchbook/libraries
+cd ~/sketchbook/libraries
+rm -rf ros_lib
+rosrun rosserial_arduino make_libraries.py ~/sketchbook/libraries
 
 
 ## CleanUp
 cd $home
-sudo rm -r -f libuvc_installation.sh
-sudo rm -r -f librealsense_build
 sudo apt autoremove -y
 sudo apt install gnome-software -y
 
 
 ## Environment setup
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/$ROS_VER/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
@@ -98,7 +92,7 @@ source ~/.bashrc
 ## Test
 
 # test ROS
-#source /opt/ros/noetic/setup.bash
+#source /opt/ros/$ROS_VER/setup.bash
 #roscd
 #roscore
 
