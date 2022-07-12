4ac089959beb5334753f4f9f7fc819595293da79
Lorenzo <uni.lorenzo.a@gmail.com>
2022-07-12 14:33:57 +0100

    32bit

--- Raspberry_Pi_OS/init.sh
+++ Raspberry_Pi_OS/init.sh
@@ -18,7 +18,7 @@ sudo apt install dkms -y
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au
 sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
-sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
+sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
 sudo make dkms_install
 
 ## Install Software
